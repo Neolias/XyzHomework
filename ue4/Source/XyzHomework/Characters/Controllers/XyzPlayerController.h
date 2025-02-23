b9112cbcaf62890edec715a84cc7b87532609329
Neolias <74720248+Neolias@users.noreply.github.com>
2025-02-23 16:35:43 +0200

    Update XyzPlayerController.h

--- ue4/Source/XyzHomework/Characters/Controllers/XyzPlayerController.h
+++ ue4/Source/XyzHomework/Characters/Controllers/XyzPlayerController.h
@@ -76,7 +76,6 @@ private:
 	void InteractWithObject();
 	void UseInventory();
 	void UseRadialMenu();
-	void ConfirmRadialMenuSegment();
 
 	UFUNCTION()
 	void OnInteractableObjectFound(FName ActionName);
